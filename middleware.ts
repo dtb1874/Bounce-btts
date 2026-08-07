76cb405c37f87a51957c7ecfdece5ecc0e0c55c4
dtb1874 <dtb1874@gmail.com>
2026-08-07 15:54:09 +0100

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,13 @@ export async function middleware(request: NextRequest) {
     {
       cookies: {
         getAll: () => request.cookies.getAll(),
-        setAll(cookiesToSet) {
+        setAll(
+          cookiesToSet: Array<{
+            name: string;
+            value: string;
+            options?: Parameters<typeof response.cookies.set>[2];
+          }>
+        ) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
